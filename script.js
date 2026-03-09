77e848b368b56518cdb669c296304a471dff3dfd
Seanghayy.Admin <seanghay.huort@wooribank.com>
2026-03-09 09:54:09 +0700

    add: connect with git hub project

--- script.js
+++ script.js
@@ -1,13 +1,50 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+const username = "seanghay-huort";
 
-anchor.addEventListener("click", function(e) {
+fetch(`https://api.github.com/users/${username}/repos`)
+.then(res => res.json())
+.then(data => {
 
-e.preventDefault();
+const container = document.getElementById("github-projects");
 
-document.querySelector(this.getAttribute("href"))
-.scrollIntoView({
-behavior: "smooth"
+data.slice(0,6).forEach(repo => {
+
+const card = document.createElement("div");
+card.className = "project";
+
+card.innerHTML = `
+<h3>${repo.name}</h3>
+<p>${repo.description || "No description provided."}</p>
+<a href="${repo.html_url}" target="_blank" class="project-btn">View Code</a>
+`;
+
+container.appendChild(card);
+
 });
+
+});
+
+
+const toggle = document.getElementById("themeToggle");
+
+toggle.addEventListener("click", () => {
+
+document.body.classList.toggle("light-mode");
+
+});
+
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+
+sections.forEach(section => {
+
+const top = section.getBoundingClientRect().top;
+
+if(top < window.innerHeight - 100){
+
+section.classList.add("visible");
+
+}
 
 });
 
